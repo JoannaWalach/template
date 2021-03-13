9b8df323cc02af438ff1e33bd504c57be916432a
Joanna <joannawalach@poczta.fm>
2021-03-13 20:42:05 +0100

    is active in MenuLink

--- src/components/Menu/MenuLink.js
+++ src/components/Menu/MenuLink.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-        function MenuLink({ to, title}) {
-            return (
-          
-              <a  href = {to}>{title}
-              </a>
-            );
-          }
+function MenuLink({ to, title, isActive }) {
+  const styles = {
+    backgroundColor: 'red'
+  }
+  const [active, setActive] = useState(isActive);
+  const clickHandler = () => {
+    setActive(true)
+  }
+  return (
+    <>
+      {
+        active ?
+          <a style={styles} href={to} onClick={clickHandler}>{title}</a>
+        : <a href={to} title={title} onClick={clickHandler}>{title}</a>
+        }
+    </>
+  );
+}
 
 export default MenuLink;
